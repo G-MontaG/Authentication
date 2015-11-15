5ecf0249debf728e59625ddc8b083d29b45eec03
Arthur Osipenko <arthur.osipenko@gmail.com>
2015-11-15 16:52:01 +0200

    upgrade routes

--- app/routes/login.js
+++ app/routes/login.js
@@ -7,63 +7,42 @@ module.exports = (app) => {
   app.get('/login', (req, res) => {
     res.render('login.ejs', {message: ''});
   });
-  app.post('/login/:type', (req, res) => {
-    if (req.params.type === "local") {
-      new Promise((resolve, reject) => {
-        User.findOne({"email": req.body.email}, (err, user) => {
-          if (err) {
-            reject(console.error(err));
+  app.post('/login/local', (req, res) => {
+    new Promise((resolve, reject) => {
+      User.findOne({"email": req.body.email}, (err, user) => {
+        if (err) {
+          reject(console.error(err));
+        }
+        if (!user) {
+          reject(res.render('login.ejs', {message: 'email not found'}));
+        }
+        else {
+          if (!user.isValidPassword(req.body.password)) {
+            reject(res.render('login.ejs', {message: 'password not correct'}));
+          } else {
+            resolve(user);
           }
-          if (!user) {
-            reject(res.render('login.ejs', {message: 'email not found'}));
-          }
-          else {
-            if (!user.isValidPassword(req.body.password)) {
-              reject(res.render('login.ejs', {message: 'password not correct'}));
-            } else {
-              resolve(user);
-            }
-          }
-        });
-      }).then((user) => {
-        Token.remove({'user_id': user._id}, (err, token) => {
-          if (err) {
-            console.error(err);
-          }
-        });
-        return user;
-      }).then((user) => {
-        let newToken = new Token();
-        newToken.setToken(user._id);
-        newToken.save((err, token) => {
-          if (err) {
-            console.error(err);
-          }
-          req.session.token = token.token;
-          res.redirect('/profile');
-        });
-      }).catch((err) => {
-        console.error(err);
+        }
       });
-
-    } else if (req.params.type === "google") {
-      //User.findOne({ "email": req.body.email }, (err, user) => {
-      //  if (err) {
-      //    console.error(err);
-      //  }
-      //  if (!user) {
-      //    res.render('login.ejs', { message: 'email not found'});
-      //  } else {
-      //    if (user.isValidToken('google', req.body.token)) {
-      //      req.session.user = user;
-      //      res.redirect('/profile');
-      //    } else {
-      //      res.render('login.ejs', { message: 'token not correct'});
-      //    }
-      //  }
-      //});
-    } else {
-
-    }
+    }).then((user) => {
+      Token.remove({'user_id': user._id}, (err, token) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      return user;
+    }).then((user) => {
+      let newToken = new Token();
+      newToken.setToken(user._id);
+      newToken.save((err, token) => {
+        if (err) {
+          console.error(err);
+        }
+        req.session.token = token.token;
+        res.redirect('/profile');
+      });
+    }).catch((err) => {
+      console.error(err);
+    });
   });
 };
--- app/routes/signup.js
+++ app/routes/signup.js
@@ -7,68 +7,39 @@ module.exports = (app) => {
   app.get('/signup', (req, res) => {
     res.render('signup.ejs', {message: ''});
   });
-  app.post('/signup/:type', (req, res) => {
-    if (req.params.type === "local") {
-      new Promise((resolve, reject) => {
-        User.findOne({"email": req.body.email}, (err, user) => {
-          if (err) {
-            reject(console.error(err));
-          }
-          if (user) {
-            reject(res.render('signup.ejs', {message: 'email not available'}));
-          }
-          else {
-            let newUser = new User({
-              name: req.body.name,
-              email: req.body.email
-            });
-            newUser.setPassword(req.body.password);
-            newUser.save((err, user) => {
-              if (err) {
-                console.error(err);
-              }
-              resolve(user);
-            });
-          }
-        });
-      }).then((user) => {
-        let newToken = new Token();
-        newToken.setToken(user._id);
-        newToken.save((err, token) => {
-          if (err) {
-            console.error(err);
-          }
-          req.session.token = token.token;
-          res.redirect('/profile');
-        });
+  app.post('/signup/local', (req, res) => {
+    new Promise((resolve, reject) => {
+      User.findOne({"email": req.body.email}, (err, user) => {
+        if (err) {
+          reject(console.error(err));
+        }
+        if (user) {
+          reject(res.render('signup.ejs', {message: 'email not available'}));
+        }
+        else {
+          let newUser = new User({
+            name: req.body.name,
+            email: req.body.email
+          });
+          newUser.setPassword(req.body.password);
+          newUser.save((err, user) => {
+            if (err) {
+              console.error(err);
+            }
+            resolve(user);
+          });
+        }
       });
-    } else if (req.params.type === "google") {
-      //User.findOne({ "email": req.body.email }, (err, user) => {
-      //  if (err) {
-      //    console.error(err);
-      //  }
-      //  if (user) {
-      //    res.render('signup.ejs', { message: 'you already have account' });
-      //  } else {
-      //    newUser = new User({
-      //      name: req.body.name,
-      //      email: req.body.email,
-      //      google: {
-      //        id: req.body.id,
-      //        token: req.body.token
-      //      }
-      //    });
-      //    newUser.save((err, user) => {
-      //      if (err) {
-      //        console.error(err);
-      //      }
-      //      req.session.user = user;
-      //      res.redirect('/profile');
-      //    });
-      //  }
-      //});
-    } else {
-
-    }
+    }).then((user) => {
+      let newToken = new Token();
+      newToken.setToken(user._id);
+      newToken.save((err, token) => {
+        if (err) {
+          console.error(err);
+        }
+        req.session.token = token.token;
+        res.redirect('/profile');
+      });
+    });
   });
 };
